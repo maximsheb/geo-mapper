bd24f3e3a22d8fa8b72f687482db61b4449cdd81
Maxim Shebunyaev <maximsheb1@gmail.com>
2024-11-19 23:00:45 +0100

    add stress and load test add grafana, prometheus, locust add compose file for test run and up

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 import uvicorn
 
+from prometheus_fastapi_instrumentator import Instrumentator
+
 from app.settings import app_config
 from app.utils.fastapi_app import app
 
+Instrumentator().instrument(app).expose(app)
 
 if __name__ == "__main__":
     uvicorn.run(app, host=app_config.HOST, port=app_config.PORT)
